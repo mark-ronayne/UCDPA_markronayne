6b9272926cdc62e7ce2e0e935f82f23f4217ed7c
Mark Ronayne <ronaynem1@gmail.com>
2021-12-19 21:41:42 +0100

    Test

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+#importing the two csv files for use in PyCharm
+
 import pandas as pd
 
 df = pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project Data\FraudTest.csv")
-df1= pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project Data\FraudTrain.csv")
+# df1 = pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project Data\FraudTrain.csv")
+# print(df.head())
 
-print(df1.head())
-print(df.head())
+
+
+# identifying the unique categories which are present in the data
+category_columns = df['category'].unique()
+
+# printing category columns for reference
+print(category_columns)
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#looping over the unique categories to find the mean transaction amount per category and presenting this in a bar plot
+
 fig, ax = plt.subplots()
 
+for category in category_columns :
+    category_df = df[df['category'] == category]
+    ax.bar(category, category_df['amt'].mean())
+
+ax.set_xticklabels(category_columns, rotation=90)
 plt.show()
