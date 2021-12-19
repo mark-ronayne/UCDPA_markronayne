9e12d4d3d1a69560982b55d47992f7c25fb7a19f
Mark Ronayne <ronaynem1@gmail.com>
2021-12-19 12:46:45 +0100

    Test

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project Data\FraudTest.csv")
+df1= pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project Data\FraudTrain.csv")
+
+print(df1.head())
+print(df.head())
+
