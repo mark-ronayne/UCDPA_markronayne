a3859c087d9b40939fba6c8b513912e4c7034192
Mark Ronayne <ronaynem1@gmail.com>
2021-12-22 19:41:59 +0000

    Test

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ df = pd.read_csv(r"C:\Users\Mark Ronayne\Desktop\Personal\UCD\Semester 1\Project
 #finding out the type of data in each column
 #df.info()
 #describe the data
-print(df.describe())
+#print(df.describe())
+
+# splitting the trans_date_trans_time column into two, date and time respectively.
+
+#df['date'] = pd.to_datetime(df['trans_date_trans_time']).dt.date
+#df['time'] = pd.to_datetime(df['trans_date_trans_time']).dt.time
+
+
+#df.info()
+
+#print(df.head())
+
 #querying the df for the number of fraudulent transactions
 #print(df.query('is_fraud == 1'))
 
-# identifying the unique categories which are present in the data
-#category_columns = df['category'].unique()
+#identifying the unique categories which are present in the data
+category_columns = df['category'].unique()
 
 # printing category columns for reference
 #print(category_columns)
@@ -25,21 +36,18 @@ import seaborn as sns
 
 #looping over the unique categories to find the mean transaction amount per category and presenting this in a bar plot
 
-#fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 
-#for category in category_columns :
- #   category_df = df[df['category'] == category]
-#    ax.bar(category, category_df['amt'].mean())
+for category in category_columns :
+    category_df = df[df['category'] == category]
+    ax.bar(category, category_df['amt'].mean())
 
-#ax.set_xticklabels(category_columns, rotation=45)
-#ax.set_xlabel('Merchant Categories')
-#ax.set_ylabel('Average TX Amount')
+ax.set_xticklabels(category_columns, rotation=45)
+ax.set_xlabel('Merchant Categories')
+ax.set_ylabel('Average TX Amount')
 
-#plt.show()
+plt.show()
 
-# splitting the trans_date_trans_time column into two, date and time respectively, to allow the data to be time_ordered
-
-#df['trans_date'] = df['trans_date_trans_time']
 
 
 
